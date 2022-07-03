5e470189e4682417eaee2095332e39d582ae5cd6
8zca <excref@gmail.com>
2022-07-03 10:15:38 +0900

    [Add] deno fresh sample

---
+++ routes/handler.tsx
@@ -0,0 +1,23 @@
+/** @jsx h */
+import { h } from "preact";
+import { Handlers } from "$fresh/server.ts";
+
+// handler利用時はセットでレンダラーが必要
+// apiで同じ構文を利用すると `Error: This page does not have a component to render.` が出る
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>Handler</h1>
+      <p>See Network.</p>
+      <p>x-custom-header: Hello is set in the response header.</p>
+    </main>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,24 @@
+/** @jsx h */
+import { h } from "preact";
+import Counter from "../islands/Counter.tsx";
+
+// 基本的にサーバでレンダリングされる
+// 個別にクライアントでレンダリングする部分のみislandsに配置するアーキテクチャ
+// see: https://jasonformat.com/islands-architecture/
+// islands配下のコンポーネントはパスカルケースしないといけない
+export default function Home() {
+  return (
+    <div>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p>
+        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
+        file, and refresh!?
+      </p>
+      <Counter start={3} />
+    </div>
+  );
+}
